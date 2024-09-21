a7158ccaa7af461af4137dadadff7432208e7588
tgoel <trisha.goel@yahoo.co.uk>
2024-09-21 21:35:12 +0100

    controller

--- src/PaymentGateway.Api/Controllers/PaymentsController.cs
+++ src/PaymentGateway.Api/Controllers/PaymentsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using PaymentGateway.Api.Models;
+using PaymentGateway.Api.Models.Requests;
 using PaymentGateway.Api.Models.Responses;
 using PaymentGateway.Api.Services;
 
@@ -10,10 +12,13 @@ namespace PaymentGateway.Api.Controllers;
 public class PaymentsController : Controller
 {
     private readonly PaymentsRepository _paymentsRepository;
+    private readonly IBankService _paymentsService;
 
-    public PaymentsController(PaymentsRepository paymentsRepository)
+
+    public PaymentsController(PaymentsRepository paymentsRepository, IBankService paymentsService)
     {
         _paymentsRepository = paymentsRepository;
+        _paymentsService = paymentsService;
     }
 
     [HttpGet("{id:guid}")]
@@ -21,6 +26,22 @@ public class PaymentsController : Controller
     {
         var payment = _paymentsRepository.Get(id);
 
+        if (payment == null)
+            return new NotFoundObjectResult(payment);
+
         return new OkObjectResult(payment);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<PostPaymentResponse>> ProcessPaymentAsync([FromBody] PostPaymentRequest request)
+    {
+        var response = await _paymentsService.ProcessPayment(request);
+
+        if (response.Status == PaymentStatus.Rejected)
+            return new BadRequestObjectResult(response);
+
+        _paymentsRepository.Add(response);
+
+        return new OkObjectResult(response);
     }
 }
--- src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
+++ src/PaymentGateway.Api/Models/Requests/PostPaymentRequest.cs
@@ -2,7 +2,6 @@
 
 public class PostPaymentRequest
 {
-    //public int CardNumberLastFour { get; set; }
     public string CardNumber { get; set; }
     public int ExpiryMonth { get; set; }
     public int ExpiryYear { get; set; }
